71135fea0b67cb054929cd4de6d2c2f219a9b6f8
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-03-29 22:32:20 +0200

    Only admins can create or delete projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
 
   root "projects#index"
 
-  resources :projects do
+  resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
 
   namespace :admin do
     root 'application#index'
+
+    resources :projects, only: [:new, :create, :destroy]
   end
 end
