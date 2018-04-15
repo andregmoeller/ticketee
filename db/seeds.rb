00bcc3d2f3fb7f95ed33ded7833c2f3a6755dacc
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-04-15 13:20:51 +0200

    Fixes the example projects' description

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,6 @@ end
 
 ['Sublime Text 3', 'Internet Explorer'].each do |name|
   unless Project.exists?(name: name)
-    Project.create!(name: name, description: 'A sample project about #{name}')
+    Project.create!(name: name, description: "A sample project about #{name}")
   end
 end
